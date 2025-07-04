2cbadb743d6b4ee2bda0447bf5cce88fe7dca616
Luan Martini <luancarlosmartini@gmail.com>
2025-07-04 17:07:44 -0300

    Bruno
    Bruno me Ajudando

---
+++ src/aula18/att3tailwind.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Divprodutos(){
+    return(
+        <>
+            <hr />
+            <h1 className="m-4 text-purple-400 font-bold">Cabeçalho do segredo do Martini</h1>
+            <hr />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './aula17/app'; 
 
+import './index.css';
+
+import Divprodutos from "./aula18/att3tailwind";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
+    
     <React.StrictMode>
+        <Divprodutos />
         <App />
     </React.StrictMode>
 );
