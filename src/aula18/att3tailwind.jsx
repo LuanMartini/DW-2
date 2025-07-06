fd200e525709fdc18baf7dd3614490a3b55c2b32
Luan Martini <luancarlosmartini@gmail.com>
2025-07-06 16:41:43 -0300

    Update att3tailwind.jsx

--- src/aula18/att3tailwind.jsx
+++ src/aula18/att3tailwind.jsx
@@ -3,7 +3,7 @@ export default function Divprodutos(){
     return(
         <>
             <hr />
-            <h1 className="m-4 text-purple-400 font-bold">Div personalizada com Tailwind</h1>
+            <h1 className="m-4 text-purple-200 font-bold">Div personalizada com Tailwind</h1>
             <hr />
         </>
     )
