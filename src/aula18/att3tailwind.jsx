5b0ff317ae6db6338bafab8d9bd5261204aa59e9
Luan Martini <luancarlosmartini@gmail.com>
2025-07-06 15:25:26 -0300

    Update att3tailwind.jsx

--- src/aula18/att3tailwind.jsx
+++ src/aula18/att3tailwind.jsx
@@ -3,7 +3,7 @@ export default function Divprodutos(){
     return(
         <>
             <hr />
-            <h1 className="m-4 text-purple-400 font-bold">Cabeçalho do segredo do Martini</h1>
+            <h1 className="m-4 text-purple-400 font-bold">Div personalizada com Tailwind</h1>
             <hr />
         </>
     )
