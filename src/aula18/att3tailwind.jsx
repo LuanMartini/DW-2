df64983dd7ec09c5f4a55e9fee840eb4bc5d1e3f
Luan Martini <luancarlosmartini@gmail.com>
2025-07-08 10:43:01 -0300

    Update att3tailwind.jsx

--- src/aula18/att3tailwind.jsx
+++ src/aula18/att3tailwind.jsx
@@ -3,9 +3,9 @@ export default function Divprodutos(){
     return(
         <>
             <hr />
-            <h1 className="m-4 text-purple-200 font-bold">Div personalizada com Tailwind</h1>
-            <h2 className="m-8 text-blue-100 ">Eu sou o rei do css </h2>
-            <h3 className="m-16 text-red-50 ">Eu sou o rei do css </h3>
+            <h1 className="m-4 text-purple-400 font-bold">Div personalizada com Tailwind</h1>
+            <h2 className="m-8 text-blue-400 font-bold">Eu sou o rei do css </h2>
+            <h3 className="m-16 text-red-400 font-bold">Eu sou o rei do css </h3>
             <hr />
         </>
     )
