4688f9467cf3c00f3a0c741bb0bb32c2f60edf24
Luan Martini <luancarlosmartini@gmail.com>
2025-07-08 10:27:05 -0300

    teste
    testeteste

--- src/aula18/att3tailwind.jsx
+++ src/aula18/att3tailwind.jsx
@@ -4,6 +4,8 @@ export default function Divprodutos(){
         <>
             <hr />
             <h1 className="m-4 text-purple-200 font-bold">Div personalizada com Tailwind</h1>
+            <h2 className="m-8 text-blue-100 ">Eu sou o rei do css </h2>
+            <h3 className="m-16 text-red-50 ">Eu sou o rei do css </h3>
             <hr />
         </>
     )
