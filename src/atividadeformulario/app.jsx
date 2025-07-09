106dd883b5f3454456d7d79b7c76473eff9bdd14
Luan Martini <luancarlosmartini@gmail.com>
2025-07-09 20:04:15 -0300

    aula 3 ajustada
    ajuste aula 3

--- src/atividadeformulario/app.jsx
+++ src/atividadeformulario/app.jsx
@@ -2,8 +2,8 @@ import Formulario from "./form";
  
 export default function App(){
     return(
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold text-center mb-6">Mini Formulário</h1>
+    <div className="min-h-screen bg-black-400 p-4">
+      <h1 className="text-2xl font-bold text-center mb-6"> Formulário</h1>
       <Formulario />
     </div>
     );
