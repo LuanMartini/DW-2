40847f555c4770ebcafafcfe98573bdaf63a4b06
Luan Martini <luancarlosmartini@gmail.com>
2025-07-08 21:23:01 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './aula18/app'; 
+// import App from './aula18/app'; 
+import App from './atividadeformulario/app'
 
 import './index.css';
 
