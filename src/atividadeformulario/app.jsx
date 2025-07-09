8a45fb4d54f191390adfc3a4cd4044fdb02f1e2c
Luan Martini <luancarlosmartini@gmail.com>
2025-07-08 21:32:19 -0300

    Update app.jsx

--- src/atividadeformulario/app.jsx
+++ src/atividadeformulario/app.jsx
@@ -2,8 +2,9 @@ import Formulario from "./form";
  
 export default function App(){
     return(
-    <>
-        <Formulario />
-    </>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-center mb-6">Mini Formulário</h1>
+      <Formulario />
+    </div>
     );
 }
